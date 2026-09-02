46a7e9b2fffd4063d997c27f4fbe0e66cfc907c2
Göran Hegenberg <goeran@hegenberg-privat.de>
2026-09-02 08:48:48 +0200

    #gg: generated DNA

--- scripts/functions/pull-request-url.js
+++ scripts/functions/pull-request-url.js
@@ -6,6 +6,8 @@
  * found in the LICENSE file in the root of this package.
  */
 
+import { execSync } from 'child_process';
+
 export function pullRequestUrl() {
   const json = execSync('gh pr view --json url', {
     encoding: 'utf-8',
