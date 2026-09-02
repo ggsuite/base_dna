9216d13a23727fe409854b0cfe4da6616d96eb67
Goeran Hegenberg <goeran@intsode.com>
2026-09-02 08:57:39 +0200

    Parameterize the copyright header and repair the org URLs in the repo guide

--- dna/doc/templates/class_template.dart
+++ dna/doc/templates/class_template.dart
@@ -1,5 +1,5 @@
 // @license
-// Copyright (c) ggsuite
+// Copyright (c) dnaCopyrightHolder
 //
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
